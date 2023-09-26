b1db56a5fd58257ca01a878eb6fa6acb13a758f0
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-09-26 19:33:52 +0100

    cambio nombre y formateo

---
+++ p02_strings/String.cc
@@ -0,0 +1,208 @@
+// Copyright (C) 2023 Francesco Marelli
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Práctica 2: Cadenas y Alfabetos
+// Autor: Francesco Marelli
+// Correo: alu0101161730@ull.edu.es
+// Fecha: 22/09/2023
+#include "String.h"
+
+/**
+ * @brief Construct a new String:: String object
+ * 
+ * @param line 
+ */
+String::String(const std::string& line) {
+    for (int i = 0; i < line.size(); ++i)
+    if (!std::iscntrl(line[i]))
+        string_.push_back(Symbol(std::string(1, line[i])));
+    size_ = string_.size();
+}
+
+
+/**
+ * @brief Construct a new String:: String object
+ * 
+ * @param string 
+ */
+String::String(const String& string) {
+    for (int i = 0; i < string.size_; ++i) {
+        string_.push_back(string.getSymbol(i));
+    }
+    size_ = string_.size();
+}
+
+
+
+/**
+ * @brief 
+ * 
+ * @param string 
+ */
+void String::setSymbol(std::vector<Symbol> string) {
+    for (int i = 0; i < string.size(); ++i) {
+        string_[i] = string[i];
+    }
+}
+
+
+/**
+ * @brief 
+ * 
+ * @param os 
+ * @param string 
+ * @return std::ostream& 
+ */
+std::ostream &operator<<(std::ostream &os, const String &string) {
+    for (int i = 0; i < string.size_; ++i) {
+        os << string.getSymbol(i);
+    }
+    return os;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @param alphabet 
+ * @return true 
+ * @return false 
+ */
+bool String::checkString(Alphabet alphabet) {
+    // comprueba que string este en alphabet
+    std::set<Symbol> alphabet_ = alphabet.getAlphabet();
+    for (int i = 0; i < size_; ++i) {
+        if (alphabet_.find(string_[i]) == alphabet_.end()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @return Alphabet 
+ */
+std::set<Symbol> String::extractAlphabet() {
+    std::set<Symbol> alphabet;
+    if (size() == 0) {
+        return alphabet;
+    }
+    if (string_[0] == Symbol(kEpsilon)) {
+        return alphabet;
+    }
+    for (const Symbol& symbol : string_) {
+        if (symbol != Symbol(kEpsilon)) {
+            alphabet.insert(symbol);
+        }
+    }
+    return alphabet;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @return unsigned 
+ */
+unsigned String::extractLength() const {
+    if (size_ == 0) {
+        return 0;
+    }
+    // Si contiene la cadena vacia es 0
+    if (string_[0] == Symbol(kEpsilon)) {
+        return 0;
+    } else {
+        return size_  - (string_[size_ - 1] == Symbol("\0"));
+    }
+}
+
+
+/**
+ * @brief 
+ * 
+ * @param string 
+ * @param alphabet 
+ * @return String 
+ */
+String String::inverse(String string, Alphabet alphabet) {
+  std::set<Symbol> alphabet_ = alphabet.getAlphabet();
+  std::vector<Symbol> inverse;
+  if (checkString(alphabet)) {
+    for (int i = 0; i < string.size_; ++i) {
+    if (alphabet_.find(string.getSymbol(i)) != alphabet_.end()) {
+      inverse.push_back(string.getSymbol(i));
+    }
+  }
+
+
+  // Invierte el vector 'inverse' sin los simbolos vacios
+  std::reverse(inverse.begin(), inverse.end());
+    return String(inverse);
+  } else {
+    std::cout << "La cadena " << string << " no pertenece al alfabeto " << std::endl;
+    return String("");
+  }
+}
+
+/**
+ * @brief 
+ * 
+ * @return std::set<String> 
+ */
+std::set<String> String::extractPrefixes() {
+    std::set<String> prefixes;
+    std::string prefix;
+    for (int i = 0; i < size_; ++i) {
+        prefix += string_[i].getSymbol();
+        prefixes.insert(String(prefix));
+    }
+    return prefixes;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @return std::set<String> 
+ */
+std::set<String> String::extractSuffixes() {
+    std::set<String> suffixes;
+    std::string suffix;
+    for (int i = size_ - 1; i >= 0; --i) {
+        suffix = string_[i].getSymbol() + suffix;
+        suffixes.insert(String(suffix));
+    }
+    return suffixes;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @param other 
+ * @return true 
+ * @return false 
+ */
+bool String::operator<(const String& other) const {
+    if (size_ < other.size_) {
+        return true;
+    }
+    if (size_ > other.size_) {
+        return false;
+    }
+    for (int i = 0; i < size_; ++i) {
+        if (string_[i] < other.string_[i]) {
+            return true;
+        } else if (string_[i].getSymbol() > other.string_[i].getSymbol()) {
+            return false;
+        }
+    }
+    return false;
+}
+
---
+++ p02_strings/String.h
@@ -0,0 +1,50 @@
+// Copyright (C) 2023 Francesco Marelli
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Práctica 2: Cadenas y Alfabetos
+// Autor: Francesco Marelli
+// Correo: alu0101161730@ull.edu.es
+// Fecha: 22/09/2023
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+
+#include "Symbol.h"
+#include "Alphabet.h"
+
+#pragma once
+
+const std::string kEpsilon = "&";
+
+class String {
+ private:
+    std::vector<Symbol> string_;
+    unsigned size_;
+
+ public:
+    explicit String(std::vector<Symbol> &string) : string_(string) { size_ = string.size(); }
+    explicit String(const std::string &line);
+    String(const String &string);
+    String() {}
+    ~String() {}
+
+    std::vector<Symbol> getSymbol() { return string_; }
+    const Symbol getSymbol(int i) const { return string_[i]; }
+    void setSymbol(std::vector<Symbol> symbol);
+    unsigned size() const { return string_.size(); }
+
+    std::set<Symbol> extractAlphabet();                //  opcode 1
+    unsigned extractLength() const;                    //  opcode 2
+    String inverse(String string, Alphabet alphabet);  //  opcode 3
+    std::set<String> extractPrefixes();                //  opcode 4
+    std::set<String> extractSuffixes();                //  opcode 5
+    bool checkString(Alphabet alphabet);
+
+    friend std::ostream &operator<<(std::ostream &os, const String &string);
+    bool operator==(const String &other);
+    bool operator<(const String &other) const;
+};
