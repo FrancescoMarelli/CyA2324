30b867a13854ce3521ff7beb3ac25da71f5befc4
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-10-15 16:04:26 +0100

    runnable

--- p05_automatas/p05_automata_simulator.cc
+++ p05_automatas/p05_automata_simulator.cc
@@ -44,7 +44,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     Automaton automaton(fileFa);
-    automaton.checkString(fileStrings, fileout);
+   // automaton.checkString(fileStrings, fileout);
 
     fileFa.close();
     fileStrings.close();
